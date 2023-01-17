ebf5bf4d4aec9f9f70de9744b4aceace76b47fc9
nancyherrera <113447349+nancyherrera@users.noreply.github.com>
2023-01-17 12:34:55 -0500

    Add files via upload

---
+++ taller ciclos anidados/punto1.js
@@ -0,0 +1,16 @@
+//series de una loteria ejemplo
+//while
+
+var x = 1;
+document.write("series de loteria  <br>")
+while(x<=3){
+    document.write("Numeros de serie  <br>")
+    var y=1;
+    while(y<=5){
+        document.write("# "+y+"<br>");
+        y++
+    }
+    x++// x = x+1, x+=1
+
+
+}
